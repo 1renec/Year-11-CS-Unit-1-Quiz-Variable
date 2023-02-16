2e188ab4066a004ab5e7f05130280cce08e32c1e
Irene <nataliachen@Irenes-MacBook-Pro.local>
2023-02-16 09:07:32 +0800

    ;;p

--- src/Variables.java
+++ src/Variables.java
@@ -3,30 +3,49 @@ public class Variables {
     public static void main(String[] args) {
 
         // 1.
+        int age = 33;
 
         // 2.
+        System.out.println(age);
 
         // 3.
+        double balance = 18.89;
 
         // 4.
+        int dollars = (int)balance;
+        System.out.print(dollars);
 
         // 5.
+        int dollarsRounded;
+        System.out.println(Math.round(balance));
 
         // 6.
+        char letter = 'A';
 
         // 7.
+        System.out.println(letter);
 
         // 8.
+        System.out.println(((int) letter));
 
         // 9.
+        letter++;
 
         // 10.
+        System.out.println(letter);
 
         // 11.
+        System.out.println(((int) letter));
 
         // 12.
         /**
-         * [type answer here]
+         * [In Q6&7 we declared and initialized a variable of type
+         * integer with the name letter to the value of A, then
+         * we printed the letter out. In Q8, we cast the variable
+         * letter to an integer then printed it out. In Q9 & 10, we
+         * incremented the variable letter using the postfix operator (++)
+         * and printed it out. in Q11 we cast the variable letter to an
+         * integer and printed it out.]
          */
 
     }
